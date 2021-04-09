408d0b450b9b2f1dbee23a9cf093aa8a602b215d
Shuanglu Dai <shud@microsoft.com>
2021-04-09 13:52:35 -0700

    bump version

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ long_description = (here / 'README.md').read_text(encoding='utf-8')
 
 setup(
     name='bertnlp',
-    version='0.0.2',
+    version='0.0.3',
     description="BERT toolkit is a Python package that performs various NLP tasks using Bidirectional Encoder Representations from Transformers (BERT) related models.",
     long_description=long_description,
     long_description_content_type="text/markdown",
